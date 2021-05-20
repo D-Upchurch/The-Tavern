7673cc6913352851c1141ed73aec90dfff755723
Dakota <dakota.c.upchurch@gmail.com>
2021-05-20 12:01:25 -0500

    Updates

--- src/components/DataManager/UrlHelper.js
+++
@@ -1,6 +0,0 @@
-export const urlHelper = () => {
-    // const url = "https://localhost:8088"
-    const url = "https://tavern-dcu.herokuapp.com"
-
-    return url
-}
--- src/components/auth/authSettings.js
+++
@@ -1,9 +0,0 @@
-// If your json-server API URL or endpoint is different, please change it below!
-export const authApi = {
-  localApiBaseUrl: "https://tavern-dcu.herokuapp.com",
-  endpoint: "users"
-}
-
-// The user id is saved under the key app_user_id in session Storage. Change below if needed!
-export const userStorageKey = "app_user_id"
-
